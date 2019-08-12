364c2198d2fc8d61417c0d7d1e1e0398e62afe21
subramaniam-kamaraj <53899241+subramaniam-kamaraj@users.noreply.github.com>
2019-08-12 20:23:50 +0530

    check whether the year is leap year or not

---
+++ set1_prob6_leap_year.py
@@ -0,0 +1,13 @@
+year=int(input())					#Enter year in number
+
+if year%4==0:
+	if year%100==0:
+		if year%400==0:
+			print("yes")
+		else:
+			print("no")
+	else:
+		print("yes")
+else:
+	print("no")
+	
